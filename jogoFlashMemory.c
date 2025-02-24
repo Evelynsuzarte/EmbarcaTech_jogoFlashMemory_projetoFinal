c2d632799ef6542e048a277a65721b3178132e1d
Evelyn Suzarte <evelynsuzarte@hotmail.com>
2025-02-24 01:23:12 -0300

    inicialização de variaveis

--- jogoFlashMemory.c
+++ jogoFlashMemory.c
@@ -1,11 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/irq.h"
+#include "hardware/pio.h"
+#include "matriz_leds.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
 
+
+#define LED_RED 12
+#define LED_GREEN 11
+#define LED_BLUE 13
+#define BUTTON_A 5
+#define BUTTON_B 6
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define I2C_PORT i2c1
+#define I2C_ADDRESS 0x3C
+#define PWM_MAX 255
+#define PWM_FREQ 20000
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 64
+#define DISPLAY_SDA 14
+#define DISPLAY_SCL 15
+#define TEMPO_DEBOUNCE 200
+#define VALOR_CENTRAL 2048
+
+
+
+void setup() {
+
+    stdio_init_all();
+
+    //LEDS
+    gpio_init(LED_RED);
+    gpio_init(LED_GREEN);
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    
+    gpio_set_function(LED_RED, GPIO_FUNC_PWM);
+    gpio_set_function(LED_BLUE, GPIO_FUNC_PWM);
+
+
+    //DISPLAY
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(DISPLAY_SDA, GPIO_FUNC_I2C);                    
+    gpio_set_function(DISPLAY_SCL, GPIO_FUNC_I2C);                    
+    gpio_pull_up(DISPLAY_SDA);                                        
+    gpio_pull_up(DISPLAY_SCL);                                      
+    ssd1306_init(&display, DISPLAY_WIDTH, DISPLAY_HEIGHT, false, I2C_ADDRESS, I2C_PORT); 
+    ssd1306_config(&display);                                              
+    ssd1306_send_data(&display);  
+    ssd1306_rect(&display, 31, 63, 8, 8, pintar, !pintar);
+    ssd1306_send_data(&display);
+
+    //botao
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+
+    gpio_init(BUTTON_B);
+    gpio_set_dir(BUTTON_B, GPIO_IN);
+    gpio_pull_up(BUTTON_B);
+
+}
+
+void callback_botao(uint gpio, uint32_t events) {
+    uint32_t tempo_atual = to_ms_since_boot(get_absolute_time());
+
+    if (tempo_atual - ultimo_tempo >= TEMPO_DEBOUNCE) {
+        if(gpio == BUTTON_A) {
+            led_R_ativado = !led_R_ativado;
+            led_B_ativado = !led_B_ativado;
+            pwm_set_enabled(slice_r, led_R_ativado); 
+            pwm_set_enabled(slice_b, led_B_ativado); 
+        }        
+        else if(gpio == JOYSTICK_BTN){
+            led_G_ativado = !led_G_ativado;
+            gpio_put(LED_GREEN, led_G_ativado);
+        }
+    
+    ultimo_tempo = tempo_atual;
+    }
+}
 
 
 int main()
 {
     stdio_init_all();
+
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &callback_botao);
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &callback_botao);
 
     while (true) {
         printf("Hello, world!\n");
