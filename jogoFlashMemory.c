5b03a699c0f0836655e4102e3a4ec4e2cd200e24
Evelyn Suzarte <evelynsuzarte@hotmail.com>
2025-02-25 23:47:25 -0300

    primeira versao

--- jogoFlashMemory.c
+++ jogoFlashMemory.c
@@ -85,35 +85,210 @@ void callback_botao(uint gpio, uint32_t events) {
     }
 }
 
+bool compara_array(int sequencia, int resposta){
+    int sequencia_quant = sizeof(sequencia) / sizeof(sequencia[0]);
+    int resposta_quant = sizeof(resposta) / sizeof(resposta[0]);
+
+    if (sequencia_quant == resposta_quant){
+        for (int i = 0; i < sequencia_quant; i++) {
+            if (sequencia[i] != resposta[i]) {
+                return false;
+            }
+        }
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int exibir_sequencia(PIO pio, uint sm, int nivel, int pontuacao){
+    int sequencia[nivel+1]; 
+    int cor;
+
+    for(i=0;i< nivel+1;i++){
+        sequencia[i] = rand() % 2;
+    }
+
+    acende_led(1);
+    for(i=0;i<nivel+1;i++){
+        cor = sequencia[i];
+        if (cor == 0){
+            triangulo_azul(PIO pio, uint sm);
+        }elif(cor == 1){
+            triangulo_vermelho(PIO pio, uint sm);
+        }
+        sleep_ms(2000);
+    }
+
+    if nivel == 5{
+        sprintf(texto_LED, "FIM DE JOGO");
+        ssd1306_draw_string(&display, texto_LED, 30, 10);
+        sprintf(texto_LED, "PONTUACAO");
+        ssd1306_draw_string(&display, texto_LED, 10, 20);
+        sprintf(texto_LED, pontuacao);
+        ssd1306_draw_string(&display, texto_LED, 10, 30);
+        ssd1306_send_data(&display);
+    }
+    acende_led(0);
+    return sequencia;
+}
 
 void mensagem_inicializa(ssd1306_t display, char texto_LED[20]){
 
     ssd1306_fill(&display, 0);
     sprintf(texto_LED, "FLASH MEMORY");
     ssd1306_draw_string(&display, texto_LED, 10, 10);
+    sprintf(texto_LED, "MEMORIZE A SEQUENCIA");
+    ssd1306_draw_string(&display, texto_LED, 10, 15);
     ssd1306_send_data(&display);
-    sprintf(texto_LED, "INICIALIZE A SEQUENCIA");
-    ssd1306_draw_string(&display, texto_LED, 10, 10);
-    ssd1306_send_data(&display);
-    sprintf(texto_LED, "ESPERE 10 SEGUNDOS E O JOGO JA VAI COMECAR");
-    ssd1306_draw_string(&display, texto_LED, 10, 10);
-    ssd1306_send_data(&display);
+    sleep_ms(3000);
+    ssd1306_fill(&display, 0);
     sprintf(texto_LED, "APERTE A PARA AZUL E B PARA VERDE");
     ssd1306_draw_string(&display, texto_LED, 10, 10);
+    sprintf(texto_LED, "ESPERE 10 SEGUNDOS E O JOGO JA VAI COMECAR");  
+    ssd1306_draw_string(&display, texto_LED, 10, 20);
     ssd1306_send_data(&display);
+    sleep_ms(10000);
+}
+
+void triangulo_azul(PIO pio, uint sm){
+    Matriz_leds_config azul = {
+        //  Coluna 0          Coluna 1          Coluna 2          Coluna 3          Coluna 4
+        // R    G    B       R    G    B       R    G    B       R    G    B       R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.2}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.2}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.2}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.2}, {0.0, 0.0, 0.2}, {0.0, 0.0, 0.2}, {0.0, 0.0, 0.2}, {0.0, 0.0, 0.2}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}  // Linha 4
+    };
+    
+   imprimir_desenho(azul, pio, sm);
+}
+
+void triangulo_vermelho(PIO pio, uint sm){
+    Matriz_leds_config vermelho = {
+        //  Coluna 0          Coluna 1          Coluna 2          Coluna 3          Coluna 4
+        // R    G    B       R    G    B       R    G    B       R    G    B       R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.2, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.2, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.2, 0.0, 0.0}, {0.2, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.2, 0.0}, {0.2, 0.0, 0.0}, {0.2, 0.0, 0.0}, {0.2, 0.0, 0.0}, {0.2, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}  // Linha 4
+    };
+    
+   imprimir_desenho(vermelho, pio, sm);
+}
+
+void apaga_matriz(PIO pio, uint sm){
+    Matriz_leds_config apaga = {
+        //  Coluna 0          Coluna 1          Coluna 2          Coluna 3          Coluna 4
+        // R    G    B       R    G    B       R    G    B       R    G    B       R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}  // Linha 4
+    };
+
+    imprimir_desenho(apaga, pio, sm);
+}
+
+void acende_led(int led){
+    switch (led){
+    case 1: 
+        gpio_put(LED_RED, 1);
+        gpio_put(LED_GREEN, 1);
+        break;
+    case 2:
+        gpio_put(LED_GREEN, 1);
+        break;
+    case 3:
+        gpio_put(LED_RED, 1);
+        break;
+    case default:
+        gpio_put(LED_GREEN, 0);
+        gpio_put(LED_RED, 0);
+        break;
+    }
 }
 
 int main()
 {
     stdio_init_all();
+    PIO pio = pio0;
+    uint sm = configurar_matriz(pio);
+
 
     gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &callback_botao);
     gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &callback_botao);
 
     mensagem_inicializa(display, texto_LED);
 
+    int nivel = 1;
+    int pontuacao;
+    bool array_igual;
+
+    
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        int sequencia_resp; 
+        int sequencia; 
+
+        sequencia = exibir_sequencia(pio, uint sm, int nivel, int pontuacao);
+
+        apaga_matriz(pio, sm);
+
+        sprintf(texto_LED, "RESPONDA A SEQUENCIA");
+        ssd1306_draw_string(&display, texto_LED, 10, 10);
+        sprintf(texto_LED, "A- AZUL");
+        ssd1306_draw_string(&display, texto_LED, 10, 20);
+        sprintf(texto_LED, "B- VERMELHO");
+        ssd1306_draw_string(&display, texto_LED, 10, 30);
+        ssd1306_send_data(&display);
+
+        int quantidade = sizeof(sequencia) / sizeof(sequencia[0]);
+
+        if (botaoA_pressionado == true){
+            if quantidade == 0{
+                sequencia_resp[0] == 0;
+            }else{
+                sequencia_resp[quantidade-1] == 0;
+            }
+            botaoA_pressionado == false;
+
+        } else if (botaoB_pressionado == true){
+            if quantidade == 0{
+                sequencia_resp[0] == 1;
+            }else{
+                sequencia_resp[quantidade-1] == 1;
+            }
+            botaoB_pressionado == false;
+        }
+
+        array_igual = compara_array(sequencia, sequencia_resp);
+
+        if (array_igual){
+            acende_led(2);
+            pontuacao=pontuacao+1;
+            nivel=nivel+1;
+
+            ssd1306_fill(&display, 0);
+            sprintf(texto_LED, "VC ACERTOU");
+            ssd1306_draw_string(&display, texto_LED, 30, 30);
+            ssd1306_send_data(&display);
+
+        }else{
+            acende_led(3);
+            ssd1306_fill(&display, 0);
+            sprintf(texto_LED, "VC ERROU");
+            ssd1306_draw_string(&display, texto_LED, 30, 30);
+            sprintf(texto_LED, "PONTUACAO");
+            ssd1306_draw_string(&display, texto_LED, 30, 10);
+            sprintf(texto_LED, pontuacao);
+            ssd1306_draw_string(&display, texto_LED, 30, 30);
+            ssd1306_send_data(&display);
+        }
+
+        sleep_ms(2000);
+        acende_led(4);
     }
 }
